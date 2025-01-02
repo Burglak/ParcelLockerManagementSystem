459f7e3bdb0ebfc0de431aaedfc781ed8189b8a6
Cyprian Schiffer <131761951+Burglak@users.noreply.github.com>
2025-01-02 19:18:16 +0100

    Add files via upload

---
+++ archiwizacja.bat
@@ -0,0 +1,7 @@
+git init
+git config --local user.name "Cyprian"
+git config --local user.email "125201@ur.edu.pl"
+git add --all
+git commit -m "Lab"
+git archive --format=zip HEAD -o ../ObsługaSieciPaczkomatow.zip
+pause
